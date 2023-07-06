6b6eb111e1fb1d56a88334b614fdbe0862a0c7cb
MAGESHKANNAN ANNATHURAI <mageshkannanam@gmail.com>
2023-07-06 10:40:09 +0530

    Improving LCP with native img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,32 @@
-import axios from "axios"
-import Image from "next/image";
+import axios from "axios";
 import { FC } from "react";
 
 interface ICard {
-    id:number
+  id: number;
 }
 type Data = {
-    avatar: string;
-    first_name: string;
-    gender: string;
-    age: number;
-    location: {
-        country: string;
-    }
-}
-export const Card:FC<ICard> = async ({id}) => {
-    
-    const res=await axios.get(`https://dummyuser.vercel.app/users/${id}`)
- 
-    const data: Data = await res.data;
+  avatar: string;
+  first_name: string;
+  gender: string;
+  age: number;
+  location: {
+    country: string;
+  };
+};
+export const Card: FC<ICard> = async ({ id }) => {
+  const res = await axios.get(`https://dummyuser.vercel.app/users/${id}`);
+
+  const data: Data = await res.data;
   return (
-      <div className=" text-white  bg-gradient-to-r from-purple-600  to-pink-500 w-[280px] h-[400px] 
-       rounded-lg flex flex-col items-center gap-3"  >
-          <Image loading="lazy" src={data.avatar} alt="avatar" width={200} height={200} />
-          <p>Name: {data.first_name}</p>
-          <p>Gender: {data.gender}</p>
-          <p>Age: { data.age}</p>
-          <p>Location: { data.location.country}</p>
+    <div
+      className=" text-white  bg-gradient-to-r from-purple-600  to-pink-500 w-[280px] h-[400px] 
+       rounded-lg flex flex-col items-center gap-3"
+    >
+      <img src={data.avatar} alt="avatar" width={200} height={200} />
+      <p>Name: {data.first_name}</p>
+      <p>Gender: {data.gender}</p>
+      <p>Age: {data.age}</p>
+      <p>Location: {data.location.country}</p>
     </div>
-  )
-}
+  );
+};
