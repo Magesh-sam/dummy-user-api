af28358379a26bbd26085e6277b2a7ed0b5a57cd
MAGESHKANNAN ANNATHURAI <41033755+Magesh-sam@users.noreply.github.com>
2023-07-12 11:24:40 +0530

    Layout Change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       <body className={RM.className}>
         <Navbar/>
         {children}
-        <Analytics />
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
